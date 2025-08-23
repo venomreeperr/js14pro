34cf05f56357b2652726b71b77a0aa01c50fed0c
venomreeperr <snehsharmacollegematerial@gmail.com>
2025-08-23 01:34:45 +0000

    kmp

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "venom@google.com"
+var accountPassword = "12345"
+accountCity = "diu"
+let accountState;
+  
+//accountId = 2
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "daman"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,3 @@
+"use strict"; //trat all js code as newer version
+
+//alert(3+3)//we are using nodejs , not browser
