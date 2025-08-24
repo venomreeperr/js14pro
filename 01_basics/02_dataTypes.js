81cdc7f48a20e3a5a33a9c455826ee64be166719
venomreeperr <snehsharmacollegematerial@gmail.com>
2025-08-24 00:09:03 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,3 +1,24 @@
-"use strict"; //trat all js code as newer version
+"use strict"; //treat all js code as newer version
 
 //alert(3+3)//we are using nodejs , not browser
+
+console.log(3+3)
+ console.log("r")
+
+ let name = "victus"
+ let age = 29
+ let isLoggedIn = false
+ let state = null ;
+ //number=>2to the power 53
+ //bigint aka long for js
+ //string =>""
+ //boolean=> true/false
+ //null=> standalone value
+ //undefined=>
+// symbol => unique
+ 
+ // object
+
+ console.log(typeof "rtx");
+  // type of null is object
+// type of undefined is umdefined
