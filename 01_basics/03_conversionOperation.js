ce0ed3a52d53de1b8afbad261f4cd3c06ca5ed05
venomreeperr <snehsharmacollegematerial@gmail.com>
2025-08-24 03:47:41 +0000

    03.2

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,3 +21,37 @@ console.log(stringNumber);
 console.log(typeof stringNumber);
 //33
 //string
+
+//$$$$$$$operationss
+
+let value = 3
+let negValue = -value
+//console.log(negValue);=>-3
+
+let str1 = "hello"
+let str2 = "boston"
+let str3 = str1+str2
+console.log(str3);
+//output=> hello boston
+
+console.log("1"+2);
+console.log(1+"2");
+//o=>12,12
+console.log("1"+2+2);
+//0=>122
+console.log(1+2+"2");
+//0=>32
+console.log(true);
+
+let num1, num2, num3
+num1=num2=num3=2+2
+
+letgameCounter=100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
