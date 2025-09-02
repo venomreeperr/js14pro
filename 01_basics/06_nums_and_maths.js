7359b2bfcd1dbb0b706d4a0d62a86c9583aa19b9
venomreeperr <snehsharmacollegematerial@gmail.com>
2025-09-02 03:54:59 +0000

    mmath

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,37 @@
+const score=400
+console.log(score);
+//>=400
+
+const balance = new Number(100)
+console.log(balance);
+//>=Number:100
+
+console.log(balance.toSring());
+//>=100
+console.log(balance.toFixed(2));
+//>=100.00 supposing that the input was 100.00
+
+const otherNumber=123.8966
+console.log(otherNumber.toPrecision(4));
+//>=123.9
+
+const hundreds=1000000
+console.log(hundreds.toLocaleString());
+//>=1,000,000
+
+//++++++++++++++++++++math
+console.log(Math);
+//>object
+console.log(Math.abs(-4));
+//>4
+console.log(Math.round(4.3));
+//>4
+console.log(Math.random());
+//gives random value within 0-1
+
+
+
+
+
+
+
