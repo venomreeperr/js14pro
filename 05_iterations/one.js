89a4c378a284b118167c233e6a7e02029b25c1b2
venomreeperr <snehsharmacollegematerial@gmail.com>
2025-09-24 11:21:18 +0000

    do while

---
+++ 05_iterations/one.js
@@ -0,0 +1,13 @@
+//for
+
+for (let index = 0; index < 10; index) {
+    const element =index++;
+ console.log(element);
+    
+} 
+
+// for (let i = 0; i < array.length; i) {
+//     const element = array[i]; 
+//     console.log(element);
+    
+// }
